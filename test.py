caee62d2078fc82f53a66d23e3f095846d232088
mcnabola <35931286+mcnabola@users.noreply.github.com>
2018-02-17 13:16:42 +0000

    Clean Up
    General Clean up, variable renaming and addittion of comments.

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import urllib.request, urllib.error
 
-f = open('names1.txt', 'r')
+input = open('names1.txt', 'r')
 output = open('websiteNames.txt', 'a')
 
 def htmlCheck(url):
@@ -8,46 +8,31 @@ def htmlCheck(url):
 	    conn = urllib.request.urlopen(url)
 		
     except urllib.error.HTTPError as e:
-        kk = "bad"
+        response = "bad"
         #print('HTTPError: {}'.format(e.code))
     except urllib.error.URLError as e:
         #print('URLError: {}'.format(e.reason))
-        kk = "bad"
+        response = "bad"
     else:
         #print('good')
-        kk = "good"
-    return kk	
+        response = "good"
+    return response	
 
 
-while True:   # include a end conditional -- if not text: break   https://stackoverflow.com/questions/6277107/parsing-text-file-in-python
-    text = f.readline()
-    #print(text)
+while True:  
+    text = input.readline()
 	
     if not text:
 	    break
 		
     names = text.split()
-    #print(names)
 	
-    for list in names:
-        #
-        url = ("http://www.skynet.ie/~"+list)
-        #print(""+url)
+    for username in names:
+        #  # skynet.ie/~username/ name of website format
+        url = ("http://www.skynet.ie/~"+username)
         result = htmlCheck(url)
         if result == 'good':
-            #print("Good: "  + url)
             output.write("Good: "  + url +"\n")
-			
 	
-	
-f.close()
-
-
-	
-
-
-	
-
-
-
-# skynet.ie/~username/ name of website format
+input.close()
+output.close()
