23aef805d0a87b727ebffebbf7ce8b4ae24784f4
mcnabola <35931286+mcnabola@users.noreply.github.com>
2018-02-17 01:56:38 +0000

    Rough working prototype
    Script parses a txtfile of usernames and uses the urllib module to check for valid websites and simply outputs the users that have a webpage.

---
+++ test.py
@@ -0,0 +1,51 @@
+import urllib.request, urllib.error
+
+f = open('names1.txt', 'r')
+
+def htmlCheck(url):
+    try:
+	    conn = urllib.request.urlopen(url)
+		
+    except urllib.error.HTTPError as e:
+        kk = "bad"
+        #print('HTTPError: {}'.format(e.code))
+    except urllib.error.URLError as e:
+        #print('URLError: {}'.format(e.reason))
+        kk = "bad"
+    else:
+        #print('good')
+        kk = "good"
+    return kk	
+
+
+while True:   # include a end conditional -- if not text: break   https://stackoverflow.com/questions/6277107/parsing-text-file-in-python
+    text = f.readline()
+    #print(text)
+	
+    if not text:
+	    break
+		
+    names = text.split()
+    #print(names)
+	
+    for list in names:
+        #
+        url = ("http://www.skynet.ie/~"+list)
+        #print(""+url)
+        result = htmlCheck(url)
+        if result == 'good':
+            print("Good: "  + url)
+			
+	
+	
+f.close()
+
+
+	
+
+
+	
+
+
+
+# skynet.ie/~username/ name of website format
